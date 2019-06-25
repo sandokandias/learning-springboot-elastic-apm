3c15448613020ce2dd96b10e4b26d2e43e9cb296
Sandokan <sandokandias@gmail.com>
2019-06-25 19:44:30 -0300

    Adding property to disable metrics collector

--- attach-agent.sh
+++ attach-agent.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 PID=$1
-java -jar apm-agent-attach-1.7.0.jar --pid $PID --config 'capture_body=transactions'
+java -jar apm-agent-attach-1.7.0.jar --pid $PID --config 'capture_body=transactions;metrics_interval=0s'
